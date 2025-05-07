0ed21303501a87f6807eda5d3c5b2cce82e7f6aa
SitiFatimah0403 <223846@student.upm.edu.my>
2025-05-07 13:47:50 +0000

    Update: latest code and assets for restaurant app

---
+++ gemini_result_model.dart
@@ -0,0 +1,16 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import 'gemini_result_widget.dart' show GeminiResultWidget;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class GeminiResultModel extends FlutterFlowModel<GeminiResultWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
